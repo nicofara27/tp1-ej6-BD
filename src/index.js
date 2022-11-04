cb6c47d285bd096241e4d1d17ecef59f898c2230
Nicolas Fara <nicolas.fara.romero@gmail.com>
2022-11-04 20:22:17 -0300

    Cambie el pueto que utiliza de 4001 a 4000

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import coloresRouter from './routes/colores.routes'
 
 const app = express();
 
-app.set('port', process.env.PORT || 4001);
+app.set('port', process.env.PORT || 4000);
 
 app.listen(app.get('port'), ()=>{
     console.log('Estoy en el puerto ' + app.get('port'));
